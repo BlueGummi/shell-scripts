6d57c1336f8ff5b359439f619259d214965d57ed
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-11-06 20:04:22 -0500

    pluh

---
+++ screensave.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+THRESHOLD=120  
+
+while true; do
+    for f in ~/.last_term_activity_*; do
+        [[ -f "$f" ]] || continue
+        last=$(cat "$f")
+        now=$(date +%s)
+        if (( now - last > THRESHOLD )); then
+            term=$(echo "$f" | sed "s#_dev_##" | tr "_" "/")
+            echo "clear; tclock" > "$term"
+            rm -f "$f"
+        fi
+    done
+    sleep 5
+done
+
